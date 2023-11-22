64d84cbe71c23d88b2d8c4bce273fcc14f9ec8df
Arthur Mendanha <arthurmendanha123@gmail.com>
2023-11-22 17:22:16 -0300

    Coloquei a opção de ver e desver a senha quando está sendo colocada nas telas de login e de cadastro

--- app/src/main/java/com/example/plife/ui/cadastro/CadastroUserActivity.java
+++ app/src/main/java/com/example/plife/ui/cadastro/CadastroUserActivity.java
@@ -28,6 +28,8 @@ public class CadastroUserActivity extends AppCompatActivity {
 
     private UserServiceImpl userService;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
